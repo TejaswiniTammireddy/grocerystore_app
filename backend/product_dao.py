a95100773ec8c5a1fc337fd6ba7f425407eaf4fe
TejaswiniTammireddy <tejaswinitammireddy@gmail.com>
2025-01-28 18:42:12 +0530

    grocerystore application

--- backend/product_dao.py
+++ backend/product_dao.py
@@ -31,6 +31,7 @@ def delete_product(connection,product_id):
     cursor.execute(query)
     connection.commit()
 
+
 if __name__=="__main__":
     connection = get_sql_connection()
     print(delete_product(connection,11))
